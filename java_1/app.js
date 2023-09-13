a575f25df90b2f2c940bde865d8c8dbdbd3e582d
MUHAMMAD SABIR <136722898+sabir-01@users.noreply.github.com>
2023-09-12 22:30:27 -0700

    javascript alert and variables

---
+++ java_1/app.js
@@ -0,0 +1,34 @@
+//alert 
+//alert("hellow world");
+
+// alert("Happy new \n year");
+
+// var Fullname="Muhammad sabir";
+// alert(Fullname);
+// alert("This is pakistan");
+
+
+
+// var a=20;
+// var b=30;
+// var c=a+b;
+// // alert("Your sum is :"+c);
+
+// var a=20;
+// var b=30;
+// var c=a-b;
+//  alert("Your sub is = "+c);
+
+
+// var a=20;
+// var b=30;
+// var c=a*b;
+//  alert("Your mul is = "+c);
+
+
+
+// console.log("Your Num 1="+a+"\nNum 2="+b+"\nresult is ="+c);
+
+// console.log("this is pakistan \nwe all are live there \nwe feel proud");
+
+
