a7f6b1b9ae8d78c1d0e0a393f572ddbe5498dc91
MUHAMMAD SABIR <136722898+sabir-01@users.noreply.github.com>
2023-09-16 23:44:54 -0700

    Assignment 1-4

---
+++ Assignment 1-4/app.js
@@ -0,0 +1,101 @@
+// // ASsignment No 1 alert
+
+// alert("hello world");
+
+// alert("Error ! Please Enter a valid password");
+// alert("well come to js land \nHappy coading");
+
+// alert("well come to js Land");
+//  alert("Happy coading! \n prevent this page for additional dialogues box");
+// alert("hello...i  can run js to my browser console");
+// console.log("hello...i  can run js to my browser console");
+
+
+// // ASsignment No 2  variables for string
+
+// var username="sabir";
+// console.log(username);
+
+// var myname;
+// myname="Muhammad sabir";
+// console.log(myname);
+
+
+
+// var message;
+// message="hello world";
+// alert(message);
+
+// var student="muhammad sabir";
+// var clas="Bse 5A";
+// var uname="bukc";
+// alert(student);
+// alert(clas);
+// alert(uname);
+
+// var message;
+// message="pizza \npizz \npiz\n pi\np";
+// alert(message);
+
+// var email="sabir0318@gmail.com";
+// alert("My email is :"+email);
+
+// var book="A smarter way to learn JavaScript";
+// alert("I am Trying to learn from book "+book)
+
+// var myname="i am  muhammad sabir";
+// var age="i am 20 year old";
+//  var study="i study in bukc";
+//  subject="i take mobile and cloud computing subject";
+//  document.write(myname+ "\n"+age+ "\n"+study+ "\n"+subject);
+// alert(myname+ "\n"+age+ "\n"+study+ "\n"+subject);
+// console.log(myname+ "\n"+age+ "\n"+study+ "\n"+subject);
+// document.write("yes i can write java content through javascript");
+
+// // Assignment No 3  variables for number
+
+// var age=14;
+// alert("i am "+age+" year old ");
+
+// var n=10;
+// alert("You have vist this site  "+n+"  times")
+
+// var birthday=2001;
+// document.write("my birthyear is  "+birthday);
+// document.write("\n  data type should be ");
+
+// var visitorName = prompt("Please enter your name:");
+// var productTitle = prompt("Please enter the product title:");
+// var quantity = prompt("Please enter the quantity you want to order:");
+
+// var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+// document.write(message);
+
+//  Assignment No 4 leegal and illegal
+
+
+
+// var variable1, variable2, variable3;
+// // Declare 5 legal variable names
+// var myVar;
+// var _variableName;
+// var $price;
+// var camel_Case;
+// var PascalCase12;
+
+// Declare 5 illegal variable names
+// Illegal variable names typically violate JavaScript naming conventions
+// and may contain spaces or special characters.
+// var 123invalid; // Cannot start with a number
+// var my-variable; // Contains a hyphen
+// var var; // Reserved keyword
+// var my variable; // Contains a space
+// var @illegal; // Contains a special character (@)
+
+// Display variable naming rules
+// document.write("b. Variable names can only contain letters, numbers, underscores, and dollar signs ($).<br>");
+// document.write("c. Variables must begin with a letter, underscore (_), or dollar sign ($).<br>");
+// document.write("d. Variable names are case-sensitive.<br>");
+// document.write("e. Variable names should not be JS reserved keywords.<br>");
+// document.write("f. my self sabir<br>"+"i live in karachi <br>"+
+// "i passed my fsc 2021");
