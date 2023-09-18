5ffe604d3d9851ccedc70d5c644b3b585c80854a
MUHAMMAD SABIR <136722898+sabir-01@users.noreply.github.com>
2023-09-18 06:30:40 -0700

    Assignment 5 to 7

---
+++ Assignment 5-7/app.js
@@ -0,0 +1,120 @@
+
+// var a=prompt("Enter the value of a :")
+
+// var b=prompt("Enter the value of b :")
+
+// var c=parseInt(a)+parseInt(b);
+// document.write("sum of "+ a+ "and "+b+" is :",c);
+
+// var a=prompt("Enter the value of a :")
+
+// var b=prompt("Enter the value of b :")
+
+// var c=parseInt(a)-parseInt(b);
+// document.write("sub of "+ a+ "and "+b+" is :",c);
+
+
+//  var a=prompt("Enter the value of a :")
+
+// var b=prompt("Enter the value of b :")
+
+// var c=parseInt(a)*parseInt(b);
+// document.write("mul of "+ a+ "and "+b+" is :",c);
+
+
+// var a=prompt("Enter the value of a :")
+
+// var b=prompt("Enter the value of b :")
+
+// var c=parseInt(a)/parseInt(b);
+// document.write("div of "+ a+ "and "+b+" is :",c);
+
+
+//  var a=prompt("Enter the value of a :")
+//  var b=prompt("Enter the value of b :")
+//  var c=parseInt(a)%parseInt(b);
+//  document.write("div of "+ a+ "and "+b+" is :",c);
+
+// var num;
+// document.write(num+"<br>");
+// num=3;
+// document.write(num+"<br>");
+// num++;
+// document.write(num+"<br>");
+// num+=7;
+// document.write(num+"<br>");
+// num--;
+// document.write(num+"<br>");
+// num=num%3;
+// document.write(num+"<br>");
+// var oneticket=600;
+// var ticket=prompt("Enter The price of u want to buy :");
+// var total=parseInt(oneticket)*parseInt(ticket);
+// document.write("total coast of buying a 5 ticket of movie is :",total);
+
+
+// var celsiusTemperature = 30; 
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// console.log(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F");
+//  document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F <br><br>");
+// var fahrenheitTemperature2 = 86; 
+// var celsiusTemperature2 = (fahrenheitTemperature2 - 32) * 5/9;
+// document.write(fahrenheitTemperature2 + "°F is " + celsiusTemperature2 + "°C");
+
+// var priceofoneitem=560 ,quantity=2;
+// document.write("Price of item  1 is 560 <br>");
+// document.write("quantity of item  1 is 2 <br>");
+// var itemonetotal =parseInt(priceofoneitem)*parseInt(quantity);
+
+// var priceoftwoitem=200 ,quantity2=5;
+// var itemtowtotal =parseInt(priceoftwoitem)*parseInt(quantity2);
+
+// document.write("Price of item  2 is 200 <br>");
+// document.write("quantity of item  2 is 5<br><br> <br>");
+// var total=itemonetotal+itemtowtotal+150;
+// document.write("shipping charges is 150 <br><br><br>");
+//  document.write("total cost of your order is :",total +"PKR")
+
+// document.write("Total marks :1200 <br>");
+// document.write("Marks obtained :900 <br>");
+// var per=((900*100)/1200);
+// document.write("percentage :",per+"%");
+
+// var currency=(10*104)+(25*28);
+//  document.write("Currency in PKR<br><br><br>");
+//  document.write("Total currancy in PKR is :",currency);
+// var num=5;
+// num=num+5;
+// num=num*10;
+// num=num/2;
+// alert(num);
+
+// var num2=(((5+5)*10)/2);
+// document.write(num2);
+// document.write("Age calculator<br><br><br>");
+
+// var Currentyear=prompt("Enter current year")
+//  document.write("Current year: ",Currentyear);
+// var birth=prompt("Enter current year ")
+// document.write("<br>Birth year  : ",birth+"<br>");
+// var real=Currentyear-birth;
+// document.write("<br>They are Either ",real ,"Or 24");
+
+// var radius=prompt("Enter value of radius <br>");
+// document.write("Radius of circle is ",radius);
+// var circum=(2*radius*3.142);
+// document.write("<br> the circumference is ",circum);
+// var area=(3.142*radius*radius)
+// document.write("<br> The area is  ",area);
+var favoriteSnack = "chocolate bars";
+
+var currentAge = 30; 
+var maximumAge = 80; 
+var amountPerDay = 2;
+
+var yearsRemaining = maximumAge - currentAge;
+var snacksPerYear = amountPerDay * 365;
+var totalSnacksNeeded = snacksPerYear * yearsRemaining;
+
+document.write("You will need " + totalSnacksNeeded + " " + favoriteSnack + " to last you until the ripe old age of " + maximumAge + ".");
+
